7c00acde9e17979d670287c031061d5c6dd2c402
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-03-12 20:41:09 +0000

    Excerise 3: ClosetAdapter refactored to its own class

--- app/src/main/java/ie/setu/project/activities/ClothingListActivity.kt
+++ app/src/main/java/ie/setu/project/activities/ClothingListActivity.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
 import ie.setu.project.R
+import ie.setu.project.adapters.ClosetAdapter
 import ie.setu.project.closet.main.MainApp
 import ie.setu.project.databinding.ActivityClothingListBinding
 import ie.setu.project.databinding.ActivityMainBinding
@@ -70,30 +71,3 @@ class ClothingListActivity : AppCompatActivity() {
 }
 
 
-
-class ClosetAdapter constructor(private var closetItems: List<ClosetOrganiserModel>) :
-    RecyclerView.Adapter<ClosetAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardClothingBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val closetItem = closetItems[holder.adapterPosition]
-        holder.bind(closetItem)
-    }
-
-    override fun getItemCount(): Int = closetItems.size
-
-    class MainHolder(private val binding : CardClothingBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(closetItem: ClosetOrganiserModel) {
-            binding.clothingItemTitle.text = closetItem.title
-            binding.clothingDescription.text = closetItem.description
-        }
-    }
-}
