8778eae5cf110f2e065b01573a25de402ac817f2
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-03-13 16:59:26 +0000

    Passing Data - Parcelable

--- app/src/main/java/ie/setu/project/models/ClosetOrganiserModel.kt
+++ app/src/main/java/ie/setu/project/models/ClosetOrganiserModel.kt
@@ -1,6 +1,10 @@
 package ie.setu.project.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ClosetOrganiserModel(
     var title: String = "",
     var description: String = ""
-)
+) : Parcelable
