2fe72752d0d80c5e8e085c88b52e02dbfd82c70d
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-05-06 21:40:39 +0100

    JUnit Create test

--- app/src/test/java/ie/setu/project/ClothingMemStoreTest.kt
+++ app/src/test/java/ie/setu/project/ClothingMemStoreTest.kt
@@ -3,6 +3,8 @@ package ie.setu.project.models.clothing
 import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
+import java.time.LocalDate
+import java.util.Date
 
 class ClothingMemStoreTest {
 
@@ -16,5 +18,20 @@ class ClothingMemStoreTest {
     @Test
     fun `findAll returns empty list initially`() {
         assertEquals(0, store.findAll().size)
+    }
+
+    @Test
+    fun `create adds item to store`() {
+        val item = ClosetOrganiserModel(
+            title = "Test Shirt",
+            description = "Test Description",
+            colourPattern = "Blue",
+            size = "8",
+            season = "Summer",
+            lastWorn = Date()
+        )
+        store.create(item)
+        assertEquals(1, store.findAll().size)
+        assertEquals(item, store.findAll().first())
     }
 }
