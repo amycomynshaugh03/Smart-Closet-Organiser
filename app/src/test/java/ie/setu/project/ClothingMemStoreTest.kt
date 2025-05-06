10af17329c88005ac46a993c59cac618c24de377
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-05-06 21:47:35 +0100

    JUnit Update test

--- app/src/test/java/ie/setu/project/ClothingMemStoreTest.kt
+++ app/src/test/java/ie/setu/project/ClothingMemStoreTest.kt
@@ -4,6 +4,7 @@ import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
 import java.time.LocalDate
+import java.util.Calendar
 import java.util.Date
 
 class ClothingMemStoreTest {
@@ -33,5 +34,39 @@ class ClothingMemStoreTest {
         store.create(item)
         assertEquals(1, store.findAll().size)
         assertEquals(item, store.findAll().first())
+    }
+
+    @Test
+    fun `update modifies existing item`() {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_YEAR, -1)
+        val yesterday = calendar.time
+
+        val originalItem = ClosetOrganiserModel(
+            id = 1,
+            title = "Original",
+            description = "Original Description",
+            colourPattern = "Blue",
+            size = "8",
+            season = "Winter",
+            lastWorn = yesterday
+        )
+        store.create(originalItem)
+
+        val updatedItem = ClosetOrganiserModel(
+            id = 1,
+            title = "Updated",
+            description = "Updated Description",
+            colourPattern = "Green",
+            size = "10",
+            season = "Summer",
+            lastWorn = Date()
+        )
+        store.update(updatedItem)
+
+        val foundItem = store.findAll().first()
+        assertEquals("Updated", foundItem.title)
+        assertEquals("Updated Description", foundItem.description)
+        assertEquals("Summer", foundItem.season)
     }
 }
