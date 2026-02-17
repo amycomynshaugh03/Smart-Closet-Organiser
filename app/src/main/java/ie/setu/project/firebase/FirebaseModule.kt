5e66b9155139c72049682eb39f5c30622a998334
Amy Comyns Haugh <comynsamy03@gmail.com>
2026-02-17 18:49:03 +0000

    Firebase Starter Classes added

---
+++ app/src/main/java/ie/setu/project/firebase/FirebaseModule.kt
@@ -0,0 +1,24 @@
+package ie.setu.project.firebase
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ie.setu.project.firebase.auth.AuthRepository
+import ie.setu.project.firebase.services.AuthService
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+
+    @Provides
+    fun provideAuthRepository(auth: FirebaseAuth):
+            AuthService = AuthRepository(firebaseAuth = auth)
+
+}
