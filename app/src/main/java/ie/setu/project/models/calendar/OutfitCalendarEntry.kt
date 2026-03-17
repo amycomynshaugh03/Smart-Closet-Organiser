3d598a7a15aaa675c54f27cc390744df61bc9407
Amy Comyns Haugh <comynsamy03@gmail.com>
2026-03-17 19:25:35 +0000

    Add OutfitCalendarEntry Model

---
+++ app/src/main/java/ie/setu/project/models/calendar/OutfitCalendarEntry.kt
@@ -0,0 +1,8 @@
+package ie.setu.project.models.calendar
+
+data class OutfitCalendarEntry(
+    val dateKey: String = "",
+    val outfitId: Long = 0L,
+    val outfitTitle: String = "",
+    val note: String = ""
+)
